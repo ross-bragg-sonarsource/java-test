80c4f67dbbb5a76f4fe166641378cdc80086eba8
test <test@test.com>
2022-09-16 11:49:21 +0200

    change aut

--- src/main/java/Main4.java
+++ src/main/java/Main4.java
@@ -1,6 +1,16 @@
-import org.apache.commons.lang.SystemUtils;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Main4 {
+  private static final Pattern EMAIL_PATTERN = Pattern.compile("<(\\S*?)>");
+  private static final String AUTHOR_EMAIL = "author-mail ";
 
+  public static void main(String [] args) {
+
+    Matcher matcher = EMAIL_PATTERN.matcher("author-mail koko");
+    if (!matcher.find(AUTHOR_EMAIL.length()) || matcher.groupCount() != 1) {
+      throw new IllegalStateException("Couldn't parse author email from: ");
+    }
+  }
 
 }
