ecfffe8f895e2fde2c17f24c98b878d89e81c594
Ross Bragg <ross.bragg@sonarsource.com>
2025-02-13 09:58:25 -0600

    Revert "here are some other changes"
    This reverts commit 545a9364990a56435fc0f2d31d6f879981ea5c13.

--- src/main/java/Main4.java
+++ src/main/java/Main4.java
@@ -1,11 +1,6 @@
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import javax.servlet.ServletInputStream;
-import javax.servlet.http.Cookie;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
 public class Main4 {
   private static final Pattern EMAIL_PATTERN = Pattern.compile("<(\\S*?)>");
   private static final String AUTHOR_EMAIL = "author-mail ";
